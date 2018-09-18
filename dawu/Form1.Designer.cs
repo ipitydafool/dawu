982172f5ae6afb44108ca48af29de14cb25cde8b
unknown <ipitydafool@protonmail.com>
2018-09-17 23:45:36 -0400

    first commit

---
+++ dawu/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace dawu {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInFile = new System.Windows.Forms.TextBox();
+            this.split = new System.Windows.Forms.SplitContainer();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.split)).BeginInit();
+            this.split.Panel1.SuspendLayout();
+            this.split.Panel2.SuspendLayout();
+            this.split.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Vidya File:";
+            // 
+            // txtInFile
+            // 
+            this.txtInFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtInFile.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.txtInFile.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystem;
+            this.txtInFile.Location = new System.Drawing.Point(73, 6);
+            this.txtInFile.Name = "txtInFile";
+            this.txtInFile.Size = new System.Drawing.Size(474, 20);
+            this.txtInFile.TabIndex = 1;
+            // 
+            // split
+            // 
+            this.split.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.split.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.split.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.split.Location = new System.Drawing.Point(15, 32);
+            this.split.Name = "split";
+            this.split.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // split.Panel1
+            // 
+            this.split.Panel1.Controls.Add(this.textBox1);
+            // 
+            // split.Panel2
+            // 
+            this.split.Panel2.Controls.Add(this.pictureBox1);
+            this.split.Size = new System.Drawing.Size(598, 406);
+            this.split.SplitterDistance = 167;
+            this.split.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(596, 165);
+            this.textBox1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(596, 233);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRun.Location = new System.Drawing.Point(553, 4);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(59, 23);
+            this.btnRun.TabIndex = 3;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 450);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.split);
+            this.Controls.Add(this.txtInFile);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Aww Shit B, How you Like me now? Mad Neural Units be meltin like swiss on tuna.";
+            this.split.Panel1.ResumeLayout(false);
+            this.split.Panel1.PerformLayout();
+            this.split.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.split)).EndInit();
+            this.split.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInFile;
+        private System.Windows.Forms.SplitContainer split;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnRun;
+    }
+}
+
---
+++ dawu/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Accord.Video.FFMPEG;
+
+
+namespace dawu {
+    public partial class Form1 : Form {
+
+        Dictionary<string, int> dIChing = new Dictionary<string, int>() {
+            { "111111",1 },
+            {"000000", 2},
+            {"010001", 3},
+            {"100010", 4},
+            {"010111", 5},
+            {"111010", 6},
+            {"000010", 7},
+            {"010000", 8},
+            {"110111", 9},
+            {"111011", 10},
+            {"000111", 11},
+            {"111000", 12},
+            {"111101", 13},
+            {"101111", 14},
+            {"000100", 15},
+            {"001000", 16},
+            {"011001", 17},
+            {"100110", 18},
+            {"000011", 19},
+            {"110000", 20},
+            {"101001", 21},
+            {"100101", 22},
+            {"100000", 23},
+            {"000001", 24},
+            {"111001", 25},
+            {"100111", 26},
+            {"100001", 27},
+            {"011110", 28},
+            {"010010", 29},
+            {"101101", 30},
+            {"011100", 31},
+            {"001110", 32},
+            {"111100", 33},
+            {"001111", 34},
+            {"101000", 35},
+            {"000101", 36},
+            {"110101", 37},
+            {"101011", 38},
+            {"010100", 39},
+            {"001010", 40},
+            {"100011", 41},
+            {"110001", 42},
+            {"011111", 43},
+            {"111110", 44},
+            {"011000", 45},
+            {"000110", 46},
+            {"011010", 47},
+            {"010110", 48},
+            {"011101", 49},
+            {"101110", 50},
+            {"001001", 51},
+            {"110100", 53},
+            {"100100", 52},
+            {"001011", 54},
+            {"001101", 55},
+            {"101100", 56},
+            {"110110", 57},
+            {"011011", 58},
+            {"110010", 59},
+            {"010011", 60},
+            {"110011", 61},
+            {"001100", 62},
+            {"010101", 63},
+            {"101010", 64 }
+        };
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        char iChing2char(List<string> line) {
+            int x = 0;
+            foreach (var s in line) {
+                x += dIChing[s];
+            }
+            return (char)x;
+        }
+
+        private void btnRun_Click(object sender, EventArgs e) {
+            VideoFileReader vfr = new VideoFileReader();
+            if (!File.Exists(txtInFile.Text)) {
+                MessageBox.Show("The file in the Vidya File box does not exist. Please type a path to an .mp4 file in the Vidya File box then click run.");
+                return;
+            }
+            vfr.Open(txtInFile.Text);
+            List<char> res = new List<char>();
+            for (int i = 0; i < vfr.FrameCount; i++) {
+                Application.DoEvents();
+                Bitmap b = vfr.ReadVideoFrame(i);
+                Pen green = new Pen(Color.Green), red = new Pen(Color.Red);
+                Graphics g = Graphics.FromImage(b);
+                int ys = 180;
+                pictureBox1.Image = b;
+                int h = 24, w = 18;
+                List<List<System.Drawing.Point>> lines = new List<List<System.Drawing.Point>>();
+                for (int j = ys + 3; j < b.Height; j++) {
+                    int x = 0;
+                    for (int k = 0; k < b.Width; k++) {
+                        Color c = b.GetPixel(k, j);
+                        if (c.R < 127 && c.B < 127 && c.G < 127) x++;
+                    }
+                    if (x > 10) {
+                        lines.Add(new List<System.Drawing.Point>() {
+                            new System.Drawing.Point(0,j),
+                            new System.Drawing.Point(b.Width, j)
+                        });
+                        lines.Add(new List<System.Drawing.Point>() {
+                            new System.Drawing.Point(0, j + h),
+                            new System.Drawing.Point(b.Width, j + h)
+                        });
+                        List<string> line = new List<string>();
+                        List<int> starts = new List<int>();
+                        for (int a = 0; a < b.Width; a++) {
+                            int z = 0;
+                            for (int c = j; c <= j + h / 3 && b.Height > j + h; c++) {
+                                Color d = b.GetPixel(a, c);
+                                if (d.R < 127 && d.B < 127 && d.G < 127) z++;
+                            }
+                            if (z > 2) {
+                                starts.Add(a);
+                                lines.Add(new List<System.Drawing.Point>() {
+                                    new System.Drawing.Point(a,j),
+                                    new System.Drawing.Point(a, j+h)
+                                });
+                                lines.Add(new List<System.Drawing.Point>() {
+                                    new System.Drawing.Point(a+w,j),
+                                    new System.Drawing.Point(a+w, j+h)
+                                });
+                                int[] avg = new int[h];
+
+                                for (int m = j; m < j + h; m++) {
+                                    avg[m - j] = 0;
+                                    for (int n = a + w / 3; n < a + 2 * w / 3; n++) {
+                                        Color dat = b.GetPixel(n, m);
+                                        avg[m - j] += dat.R + dat.G + dat.B;
+                                    }
+                                }
+
+                                int[] bin = new int[6];
+                                int s = h / 6;
+                                for (int t = 0; t < h; t++) {
+                                    bin[t / s] += avg[t] / (w / 3) / s / 3;
+                                }
+                                string bs = "";
+                                for (int t = 0; t < bin.Length; t++) {
+                                    bs += bin[t] < 200 ? "1" : "0";
+                                }
+                                line.Add(bs);
+                                a += w * 3;
+                            }
+                        }
+                        res.Add(iChing2char(line));
+                        j += h;                    
+                    }
+                }
+                foreach (var line in lines) {
+                    g.DrawLine(red, line[0].X, line[0].Y, line[1].X, line[1].Y);
+                }
+                pictureBox1.Refresh();
+                Application.DoEvents();
+                textBox1.Text += string.Join("\r\n", string.Join("", res.ToArray()));
+                Application.DoEvents();
+            }
+        }
+    }
+}
